aab7cd896bb7b7de941c50b304f9aec3d785669f
Patrick Johnson <im@pbj.me>
2013-06-13 20:14:33 -0400

    polish.

--- index.js
+++ index.js
@@ -5,36 +5,22 @@ $(document).ready(function() {
 		var salary = userSalary.replace("$","").replace(",","");
 		
 		if(regIsNumber(userSalary)) { 
-		
-				/* run this part if userSalary is a number */
-	  			
 	       		var salValue = (parseInt(salary) / 52) / 40;
 	       		var new_number = salValue.toFixed(2);
 	       		$('#result-value').append(new_number);
-	
-				/* add some logic here to fadeOut if #result div is visible */
-				
 	       		$('#result p').fadeIn(1000).css({display: 'block'});
 	
 		} else { 
-				
-				/* if #result div is visible, hide it */
 				if($('#result-value').is(':visible')) {
 					document.getElementById('result').style.display = 'none';
 				}
-				
-				/* clear textbox */ 
 				$('.salaryInput').val(''); 
-				
-				/* show error message */
 				$('#error-message').css({display: 'block'}) ;
 				$('#error-message').fadeOut(3000);
-				
-		} /* end else */
+		}
        });
 	});
 	
-	/* check for numbers */
 	function regIsNumber(fData)	{
     		var reg = new RegExp("^[\$]?[-]?[0-9]+[\,]?[0-9]+(\.[0-9]{1,2})?$");
     		return reg.test(fData)

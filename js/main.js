ec9714fb90257f7692f9104732327685a0862ec0
Patrick Johnson <im@pbj.me>
2013-09-24 16:01:22 -0400

    fixed the broken javascripts!

--- js/main.js
+++ js/main.js
@@ -1,48 +1,20 @@
 $(document).ready(function() {
-				
-	// hide all responses on load
-	// $('.responses').hide();
-	// empty out the val input 
-	$('span.hourly-rate').empty();
-
-	
-
-
-	// form submittal and validation
 	$('form').submit(function(e){
 		e.preventDefault();
-
-		// set variables for salary input
-		var userSalary = $('.salary-input').val();
-		var salary = userSalary.replace('$','').replace(',','');
-
-		$('input').find('salary-input').empty();
-
-
-		if (regNum(userSalary)) {
-			var salaryValue = (parseInt(salary) / 52 ) / 40;
-			var newNumber = salaryValue.toFixed(2);
-
-			$('.hourly-rate').html(newNumber);
-			$('.result').fadeIn(500);
+		var salary = $('.salary-input').val().replace(/[,$]/g, '');
+		var salaryCalc = (parseInt(salary) / 52) / 40;
+		var hourlyRate = salaryCalc.toFixed(2);
+		
+		if ($.isNumeric(salary)) {
+			$('.error-message').css({ 'display' : 'none' })
+			$('.hourly-rate').html(hourlyRate);
+			$('.result').fadeIn(300);
 		} else {
-			if ( $('.hourly-rate').is(':visible')) {
-				$('.result').css("display", "none");
+			if ($('.hourly-rate').is(':visible')) {
+				$('.salary-input').val('');
+				$('.result').css({ 'display' : 'none'});
 			}
-
-			$('.salary-input').val('');
-			$('.error-message').css({"display":"block"}).fadeOut(2200);
+			$('.error-message').css({ 'display': 'block'});
 		}
-
-	}); 
-
-	// strips $ and commas from user salary input 
-
-	function regNum(fData)	{
-    		var reg = new RegExp("^[\$]?[-]?[0-9]+[\,]?[0-9]+(\.[0-9]{1,2})?$");
-    		return reg.test(fData)
-	}
-
-
+	});
 });
-	

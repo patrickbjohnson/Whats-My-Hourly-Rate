d535639723cd80dee6823e8e38a4a714b9df73c5
Patrick Johnson <patrick.johnson@expandtheroom.com>
2017-07-07 17:30:53 -0400

    wip state taxes part

--- hourly-rate/src/App.js
+++ hourly-rate/src/App.js
@@ -2,44 +2,86 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import States from 'us';
+
 class Input extends Component {
   constructor() {
     super();
     this.state = {
-      salary: 0.00,
-      hourlyRate: 0.00
+      salary: 0,
+      hourlyRate: 0
     };
 
     this.handleOnChange = this.onChangeHandler.bind(this, 'SalaryInput');
   }
 
   hourlyRate(sal) {
-    const salary = ((sal / 52) / 40).toFixed(2);
+    const rate = ((sal / 52) / 40).toFixed(2);
 
-    if (isNaN(salary)) this.setState({hourlyRate: 0});
-
-    this.setState({hourlyRate: Number(salary)});
+    this.setState({
+        hourlyRate: rate
+    });
   }
 
   onChangeHandler(ref, e) {
-    if (isNaN(e.target.value)) return;
-
+    const val = e.target.value.replace(/\,/g,'');
+    
+    if (isNaN(val)) return;
+    
     const salary = e.target.value.replace(/\,/g,'');
 
-    this.setState({salary: Number(salary)}, () => {
-        this.hourlyRate(this.state.salary);
+    this.setState({
+        salary: Number(salary)
     });
-
+    
+    this.hourlyRate(Number(salary));
   }
 
   render() {
       return (
-        <div>
-         <input type='text' placeholder="foo" onKeyPress={this.handleOnChange} ref="SalaryInput" />
+        <div className="input-wrap">
+         <input className="input" type='text' placeholder="foo" onKeyUp={this.handleOnChange} ref="SalaryInput" />
          <p>You make ${this.state.hourlyRate} an hour</p>
         </div>
       );
   }
+}
+
+class StateList extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            selectedState: ''
+        }
+
+        this.handleStateChange = this.onStateChange.bind(this, 'selectChange');
+    }
+
+    onStateChange(event) {
+        console.log(event);
+        // this.setState({
+        //     selectedState: event.target.value
+        // });
+    }
+
+    render() {
+        const states = States.STATES;
+        console.log(states);
+        return (
+            <div>
+                <select value="{this.state.selectedState}" onChange="handleStateChange" name="" id=""  ref="selectChange">
+                    <option value="null">-- Select State</option>
+                    {
+                    states.map(function( state ) {
+                        return <option value={state.name} key={state.abbr}>{state.name}</option>;
+                    })
+                }
+                </select>
+                
+            </div>
+        );
+    }
 }
 
 class App extends Component {
@@ -52,6 +94,8 @@ class App extends Component {
         </div>
 
         <Input />
+
+        <StateList />
 
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
